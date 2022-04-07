20a76b784afba42eaae97056c9d2a5ea6305aa18
Adnan <45740212+Adnan-Sabic@users.noreply.github.com>
2022-04-07 10:40:49 +0200

    Add initial backend in SpringBoot

---
+++ platformBackend/platformBackend/src/test/java/com/platformBackend/PlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platformBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
